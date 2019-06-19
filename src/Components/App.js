4027fa08b4b87b0c2b2a832bb0ac97bb54169627
Kim Drezdon <kdrezdon@gmail.com>
2019-06-18 20:02:49 -0500

    Add separate states for each default topic and search topic

--- src/Components/App.js
+++ src/Components/App.js
@@ -10,19 +10,31 @@ import NoResults from './NoResults';
 
 class App extends Component {
   state = { 
-    images: []
+    searchImages: [],
+    amalfiImages: [],
+    tulumImages: [],
+    santoriniImages: [],
   }
 
   componentDidMount() {
-    this.performSearch();
+    this.performSearch('amalfi%20coast');
+    this.performSearch('tulum');
+    this.performSearch('santorini');
   }
 
-  performSearch(query = 'amalfi%20coast') {
+  performSearch = (query) => {
     axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=${query}&sort=interestingness-desc&safe_search=1&content_type=1&per_page=24&format=json&nojsoncallback=1`)
     .then(response => {
-      this.setState({
-        images: response.data.photos.photo
-      })
+      let responseData = response.data.photos.photo;
+      if (query === 'amalfi%20coast') {
+        this.setState({ amalfiImages: responseData })
+      } else if (query === 'tulum') {
+        this.setState({ tulumImages: responseData })
+      } else if (query === 'santorini') {
+        this.setState({ santoriniImages: responseData })
+      } else {
+        this.setState({ searchImages: responseData });
+      }
     })
     .catch(error => {
       console.log('Error fetching data', error);
@@ -33,12 +45,13 @@ class App extends Component {
     return ( 
       <BrowserRouter>
         <div className="container">
-          <Header />
+          <Route path="/" render={ (props) => <Header {...props} onSearch={this.performSearch} /> } />
           <Switch>
             <Route exact path="/" render={ () => <Redirect to="/amalficoast" /> } />
-            <Route path="/amalficoast" render={() => <ImageGallery images={this.state.images} />} />
-            <Route path="/tulum" render={() => <ImageGallery images={this.state.images} />} />
-            <Route path="/santorini" render={() => <ImageGallery images={this.state.images} />} />
+            <Route path="/amalficoast" render={() => <ImageGallery images={this.state.amalfiImages} />} />
+            <Route path="/tulum" render={() => <ImageGallery images={this.state.tulumImages} />} />
+            <Route path="/santorini" render={() => <ImageGallery images={this.state.santoriniImages} />} />
+            <Route path='/search/:topic' render={() => <ImageGallery images={this.state.searchImages} /> } />
             <Route component={NoResults} />
           </Switch>
         </div>
