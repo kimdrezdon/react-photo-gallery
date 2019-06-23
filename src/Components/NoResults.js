82776c54fb2431af34c80d8e7326eba142530259
Kim Drezdon <kdrezdon@gmail.com>
2019-06-23 15:42:27 -0500

    Change class name

--- src/Components/NoResults.js
+++ src/Components/NoResults.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NoResults = () => {
     return (  
-        <li className="not-found">
+        <li className="no-results">
             <h3>No Results Found!</h3>
             <p>Your search did not return any results. Please try again.</p>
         </li>
