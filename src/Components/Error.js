18ffe3b3da2fcd785829e75c9a237fd12e239d9a
Kim Drezdon <kdrezdon@gmail.com>
2019-06-23 15:41:45 -0500

    Modify error component

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Error = () => {
     return (  
-        <li className="not-found">
-            <h3>404 Page Not Found!</h3>
+        <div className="error">
+            <h3>404: Page Not Found!</h3>
             <p>Sorry, we could not find the page you were looking for.</p>
-        </li>
+        </div>
     );
 }
  
