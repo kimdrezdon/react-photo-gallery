96ba79d7be438ceeb2c2baf67ba2ef344bb89431
Kim Drezdon <kdrezdon@gmail.com>
2019-07-01 08:26:47 -0500

    Apply prettier

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const Error = () => {
-    /**
-     * Returns an error message when the path doesn't exist.
-     */
-    return (  
-        <div className="error">
-            <h3>404: Page Not Found!</h3>
-            <p>Sorry, we could not find the page you were looking for.</p>
-        </div>
-    );
-}
- 
+  /**
+   * Returns an error message when the path doesn't exist.
+   */
+  return (
+    <div className="error">
+      <h3>404: Page Not Found!</h3>
+      <p>Sorry, we could not find the page you were looking for.</p>
+    </div>
+  );
+};
+
 export default Error;
